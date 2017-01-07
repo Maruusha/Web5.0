339be13815601f11dd8a338e18686f7ec84b654f
Maruusha <arcturus6998@gmail.com>
2017-01-07 22:24:49 +0700

    version 1.0

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 var Nakama = {};
-Nakama.configs = {
-}
+Nakama.configs = {};
 
 window.onload = function(){
   Nakama.game = new Phaser.Game(640,960,Phaser.AUTO,'',
@@ -23,16 +22,39 @@ var preload = function(){
   Nakama.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
   Nakama.game.time.advancedTiming = true;
+
+  Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
+  Nakama.game.load.image('background', 'Assets/Map1.png');
 }
 
 // initialize the game
 var create = function(){
   Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
-  Nakama.keyboard = Nakama.game.input.keyboard; 
+  Nakama.keyboard = Nakama.game.input.keyboard;
+  Nakama.player = Nakama.game.add.sprite(
+    200,
+    200,
+    "assets",
+    "Spaceship1-Player.png"
+  );
 }
 
 // update game state each frame
-var update = function(){}
+var update = function(){
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
+    Nakama.player.position.y -=10;
+  }
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
+    Nakama.player.position.y +=10;
+  }
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
+    Nakama.player.position.x -=10;
+  }
+  if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+    Nakama.player.position.x +=10;
+  }
+
+}
 
 // before camera render (mostly for debug)
 var render = function(){}
