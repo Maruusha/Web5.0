5e86567f353d8f0747a3acd80fcbd518e6b61763
Maruusha <arcturus6998@gmail.com>
2017-07-04 18:06:43 +0700

    finish task 1

---
+++ SaaS/4-7-17/server.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const exhbs = require('express-handlebars')
+
+let app = express();
+let hbs = exhbs.create({});
+
+app.use(bodyParser.json({ extended : true}));
+app.use(bodyParser.urlencoded({ extended : true}));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.get('/', (req,res) =>{
+  res.render('home');
+})
+//question/:id
+app.get('/question/:id', (req, res) => {
+  console.log(req.params.id);
+  try {
+    questionsList = JSON.parse(fs.readFileSync('question.json', 'utf-8'));
+  } catch (exception) {
+    console.log(exception);
+    questionsList = [];
+  }
+  let result = questionsList[req.params.id].content;
+
+  res.render('question', {
+    id : req.params.id,
+    content : result
+  });
+})
+
+router.post('/ask', (req, res) => {
+  //push question
+  let questionList;
+  try {
+    questionsList = JSON.parse(fs.readFileSync('question.json', 'utf-8'));
+  } catch (exception) {
+    console.log(exception);
+    questionsList = [];
+  }
+  question = {
+    content : req.body.question
+  };
+  questionsList.push(question);
+  // hình như sai đâu đó trong cái router này
+  res.redirect(`/question/${questionsList.length}`);
+});
+
+app.listen(3000, () =>{
+  console.log('Server is running at localhost:3000');
+})
